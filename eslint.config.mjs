f6ef7bc6dd250021ad7095d710a1657c6e5a12e0
uellenberg <JonahUellenberg@hotmail.com>
2025-05-31 10:47:44 -0700

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,9 +13,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-    ...compat.extends(
-        "eslint-config-prettier",
+    ...compat.extends("eslint-config-prettier"),
+    ...tseslint.config(
+        eslint.configs.recommended,
+        tseslint.configs.recommended,
     ),
+    ...defineConfig({
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { varsIgnorePattern: "^_" },
+            ],
+            "comma-style": ["error", "last"],
+            "array-bracket-spacing": ["error", "never"],
+            "no-trailing-spaces": ["error"],
+            "spaced-comment": ["error", "always"],
+            "comma-dangle": ["error", "always-multiline"],
+            semi: ["error", "always"],
+            "object-curly-spacing": ["error", "always"],
+            "line-comment-position": ["error", "above"],
+            curly: ["error", "multi-line"],
+        },
+    }),
 ];
 
 export default eslintConfig;
